8a54d408ed6d3513af093f2c0318d1a445526a08
Bennett Kautz <bennettkautz@gmail.com>
2022-02-16 17:17:28 -0500

    finished all tasks

--- index.js
+++ index.js
@@ -39,14 +39,33 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+     this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function(){
+  return this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
 
 
+const bennett = new Person("Bennett", 24);
 
-
+bennett.eat("bagel");
+bennett.poop();
+bennett.toString();
 
 
 /*
@@ -63,9 +82,17 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+  return this.tank + gallons;
+}
+
 
 
 /*
@@ -75,18 +102,29 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ this.name = name;
+ this.age = age;
+ this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(x){
+  return `Playing with ${this.favoriteToy}`;
+}
+
+const Natalia = new Baby("Natalia", 10, "Roblox");
+
+Natalia.play();
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. If scope is global, its object referenced will be the window/console
+  2. Whenever a dot precedes a function it will always reference the object to its left
+  3. when usinng constructors, this refers to the object that is being created as a result of the constructor function
+  4. when using call/apply methods you explicity define wht 'this' refers to
 */
 
 
